450706fd2f0b2fa592c3c874ad7d2282a39653bc
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2023-09-12 17:43:54 +0000

    Deploying to gh-pages from @ UrszulaBaranowska/Ice-Cream-Project-Website@0b3dfcc2baecbff91db85d6355b4cf733eaa4308 🚀

---
+++ index.fc729e2a.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open-3]"),closeModalBtn:document.querySelector("[data-modal-close-3]"),modal:document.querySelector("[data-modal-3]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.fc729e2a.js.map
